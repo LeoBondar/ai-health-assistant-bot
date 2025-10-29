3ae2f82e640952b4834df0f40db4f2fb5cfbe1bb
Leopoldic <136648152+LeoBondar@users.noreply.github.com>
2025-10-29 13:03:27 +0300

    fix

--- bot/views/service/set_plan_exercise_type.py
+++ bot/views/service/set_plan_exercise_type.py
@@ -1,30 +1,16 @@
-from uuid import UUIDfrom uuid import UUID
+from uuid import UUID
+
+from bot.adapters.ai_health.adapter import AIHealthAdapter
+from bot.adapters.ai_health.schemas import AIASetPlanExerciseTypeCommand, AIASetPlanExerciseTypeResponse
 
 
+class SetPlanExerciseTypeView:
+    def __init__(self, adapter: AIHealthAdapter):
+        self._adapter = adapter
 
-from bot.adapters.ai_health.adapter import AIHealthAdapterfrom bot.adapters.ai_health.adapter import AIHealthAdapter
+    async def __call__(self, plan_id: UUID, exercise_type: str) -> AIASetPlanExerciseTypeResponse:
+        response = await self._adapter.set_plan_exercise_type(
+            command=AIASetPlanExerciseTypeCommand(plan_id=plan_id, exercise_type=exercise_type)
+        )
 
-from bot.adapters.ai_health.schemas import AIASetPlanExerciseTypeCommand, AIASetPlanExerciseTypeResponsefrom bot.adapters.ai_health.schemas import AIASetPlanExerciseTypeCommand, AIASetPlanExerciseTypeResponse
-
-
-
-
-
-class SetPlanExerciseTypeView:class SetPlanExerciseTypeView:
-
-    def __init__(self, adapter: AIHealthAdapter):    def __init__(self, adapter: AIHealthAdapter):
-
-        self._adapter = adapter        self._adapter = adapter
-
-
-
-    async def __call__(self, plan_id: UUID, exercise_type: str) -> AIASetPlanExerciseTypeResponse:    async def __call__(self, plan_id: UUID, exercise_type: str) -> AIASetPlanExerciseTypeResponse:
-
-        response = await self._adapter.set_plan_exercise_type(        response = await self._adapter.set_plan_exercise_type(
-
-            command=AIASetPlanExerciseTypeCommand(plan_id=plan_id, exercise_type=exercise_type)            command=AIASetPlanExerciseTypeCommand(plan_id=plan_id, exercise_type=exercise_type)
-
-        )        )
-
-        return response
         return response
